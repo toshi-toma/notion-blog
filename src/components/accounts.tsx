9a7a93b0a611b29ea4326ae13c6674128d4c8bdf
toshi-toma <toshitoma1217@gmail.com>
2021-02-03 19:47:44 +0900

    accounts design

--- src/components/accounts.tsx
+++ src/components/accounts.tsx
@@ -1,37 +1,47 @@
 const accounts = [
   {
     text: 'GitHub',
-    link: '',
+    link: 'https://github.com/toshi-toma',
   },
   {
     text: 'Blog',
+    link: '/blog',
+  },
+  {
+    text: 'はてなブログ',
+    link: 'https://toshi-toma.hatenablog.com/',
   },
   {
     text: 'Qiita',
+    link: 'https://qiita.com/toshi-toma',
   },
   {
     text: 'npm',
+    link: 'https://www.npmjs.com/~toshi-toma',
   },
   {
     text: 'Zenn',
+    link: 'https://zenn.dev/toshitoma',
   },
   {
     text: 'Twitter',
+    link: 'https://twitter.com/toshi__toma',
   },
   {
     text: 'Slides',
-  },
-  {
-    text: '🐣',
+    link: 'https://speakerdeck.com/10shi10ma',
   },
 ]
 
 export default () => (
-  <div className="accounts">
-    {accounts.map(({ text }) => (
-      <div className="feature" key={text}>
-        <h4>{text}</h4>
-      </div>
-    ))}
+  <div className="accounts-container">
+    <h3 className="accounts-header">Accounts</h3>
+    <div className="accounts">
+      {accounts.map(({ text, link }) => (
+        <div className="feature" key={text}>
+          <h4>{link ? <a href={link}>{text}</a> : text}</h4>
+        </div>
+      ))}
+    </div>
   </div>
 )
